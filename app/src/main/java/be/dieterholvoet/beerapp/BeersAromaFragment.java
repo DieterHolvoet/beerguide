9bb0f9298009764a66b51828368cf828d8a56d01
Dieter Holvoet <dieter.holvoet@gmail.com>
2015-12-26 16:37:12 +0100

    Initial commit: GUI base (Navigation drawer, tabbed ui, toolbar, FAB)

---
+++ app/src/main/java/be/dieterholvoet/beerapp/BeersAromaFragment.java
@@ -0,0 +1,19 @@
+package be.dieterholvoet.beerapp;
+
+/**
+ * Created by Dieter on 26/12/2015.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BeersAromaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.beers_aroma, null);
+    }
+}
